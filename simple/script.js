44a4ba30cb7685bf6af21ff54917b9869eefadc7
Pamela <pamesantoss@hotmail.com>
2022-02-15 10:21:56 -0300

    feat: switch to dark and light mode functionality finished

--- simple/script.js
+++ simple/script.js
@@ -1,13 +1,41 @@
 const switcher = {
   init: function() {
     switcher.changeTheme()
+    switcher.loadTheme()
+  },
+  getMainTheme: function() {
+    const themeDark = localStorage.getItem('theme-dark')
+    return themeDark
+  },
+  setTheme: function(dark) {
+    localStorage.setItem('theme-dark', dark)
+    
+    if (dark) {
+      $('body').addClass('theme-dark')
+    } else {
+      $('body').removeClass('theme-dark')
+    }
   },
   changeTheme: function() {
     const switcherButton = $('#switch-toggle-btn')
 
     switcherButton.on('click', function() {
-      $('body').toggleClass('theme-dark')
+      switcher.setTheme($(this).is(":checked"))
     })
+  },
+  loadTheme: function() {
+    const switcherButton = $('#switch-toggle-btn')
+
+    if (switcher.getMainTheme()) {
+      if (switcher.getMainTheme() === 'true') {
+        switcher.setTheme(true)
+        switcherButton.prop('checked', true)
+      } else {
+        switcher.setTheme(false)
+      }
+    } else {
+      switcher.getMainTheme(false)
+    }
   }
 }
 
